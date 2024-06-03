646fe3fe5506547cb3ecf5940ccd4d9799d0ab92
julioczrn <julinhobiu8@gmail.com>
2024-06-03 19:47:10 -0300

    alteração-continuação no projeto

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,10 +1,34 @@
 import { v4 } from 'uuid';
+import * as Yup from 'yup';
 
 import User from '../models/Users';
 
 class UserController {
     async store(request, response) {
-        const {name, email, password_hash, admin} = request.body;
+        const schema = Yup.object({
+          name: Yup.string().required(),
+          email: Yup.string().email().required(),
+          password: Yup.string().min(6).required(),
+          admin: Yup.boolean(),
+        });
+
+        try {
+          schema.validateSync(request.body, { abortEarly: false});
+        } catch(err){
+          return response.status(400).json({ error: err.erros })
+        }
+        
+        const {name, email, password, admin} = request.body;
+
+        const UserExists = await User.findOne({
+          where: {
+            email,
+          },
+        });
+
+        if (UserExists){
+          return response.status(400).json({ error: 'User already exists' })
+        }
 
         const user = await User.create({
             id: v4(),
@@ -13,7 +37,12 @@ class UserController {
             password_hash,
             admin,
             }); 
-            return response.status(201).json(user)
+            return response.status(201).json({
+              id: user.id,
+              name,
+              email,
+              admin,
+      });
     }
 }
 
